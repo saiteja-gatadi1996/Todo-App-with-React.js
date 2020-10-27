548f9699c91b9f619543e5807cc186c2ac74cdaf
SaiTeja <homehum333@outlook.com>
2020-10-27 11:59:03 +0530

    Initialize project using Create React App

---
+++ firebase.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDS6VBN4ovWPYueC6t3yTYC0-g0nDDCpHc",
+    authDomain: "todo-app-10b11.firebaseapp.com",
+    databaseURL: "https://todo-app-10b11.firebaseio.com",
+    projectId: "todo-app-10b11",
+    storageBucket: "todo-app-10b11.appspot.com",
+    messagingSenderId: "708032244421",
+    appId: "1:708032244421:web:37d65ce7c4556be41ddeac",
+    measurementId: "G-E5VV8STE94"
+  };
+  
